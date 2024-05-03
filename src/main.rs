67f817faab7294b3df1333104660af1050d5c0b3
stefano <stefano@zebedee.io>
2024-05-03 12:37:50 -0400

    removed unused

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use serde_json::{json, Value};
 use sqlx::Pool;
 use sqlx::Postgres;
 use std::error::Error;
-use std::fs::File;
-use std::io::Write;
 use std::sync::Arc;
 use tower_http::trace::{self, TraceLayer};
 use tracing::Level;
@@ -31,10 +29,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .with_target(false)
         .compact()
         .init();
-
-    // init_env();
-
-    // dotenv::dotenv().ok();
 
     let _nostr_relay = std::env::var("NOSTR_RELAY").expect("NOSTR_RELAY not set");
 
@@ -72,19 +66,3 @@ async fn main() -> Result<(), Box<dyn Error>> {
 async fn health() -> Json<Value> {
     Json(json!({ "health": "ok" }))
 }
-
-// fn init_env() {
-//     let database_url = "DATABASE_URL=";
-//     let database_url_environment = std::env::var("DATABASE_URL").expect("DATABASE_URL not set.");
-//     let database_url_environment = format!(
-//         "{}{}\nSQLX_OFFLINE=true",
-//         database_url, database_url_environment
-//     );
-
-//     let mut data_file = File::create(".env").expect("creation failed");
-//     data_file
-//         .write_all(database_url_environment.as_bytes())
-//         .expect("write failed");
-
-//     println!("Created a file .env");
-// }
